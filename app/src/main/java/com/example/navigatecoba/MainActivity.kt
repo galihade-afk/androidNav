921deb20c1dea93c7aaf192a8fff90b32607f2c1
galihade <galih.adesaputra@gmail.com>
2020-12-07 16:32:53 +0700

    first commit

---
+++ app/src/main/java/com/example/navigatecoba/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.navigatecoba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.navigatecoba.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main )
+    }
+}
